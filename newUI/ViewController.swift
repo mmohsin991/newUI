8021f56405b732b03afac38e058a44e6b31d7961
mmohsin991 <muhammad.mohsin.991@gmail.com>
2015-06-17 17:43:39 +0500

    upload new UI

---
+++ newUI/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  newUI
+//
+//  Created by Mohsin on 17/06/2015.
+//  Copyright (c) 2015 PanaCloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+
+
+}
+
